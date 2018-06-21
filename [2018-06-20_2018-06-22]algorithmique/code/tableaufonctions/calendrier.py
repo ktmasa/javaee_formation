97b55677f2eb4c7b3fb703eaac08f1654e3bcf42
vincent.courtalon <vincent.courtalon@gmail.com>
2018-06-21 17:06:24 +0200

    squelette pyhton pour calendrier

---
+++ [2018-06-20_2018-06-22]algorithmique/code/tableaufonctions/calendrier.py
@@ -0,0 +1,24 @@
+def calendrier(mois, annee):
+    pass
+
+def enTete(mois, annee):
+    pass
+
+def corps(mois, annee):
+    pass
+
+def getNomMois(mois):
+    pass
+
+def estBissextile(annee):
+    pass
+
+def nbJoursMois(mois, annee):
+    pass
+
+def jourDebutMois(mois, annee):
+    pass
+
+
+annee = int(input("annee (apres 1800): "))
+mois = int(input("mois de 1 à 12: "))
