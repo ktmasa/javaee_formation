3395e7bfec8bc940b379566c963e0de8d06c5df6
vincent.courtalon <vincent.courtalon@gmail.com>
2018-06-22 12:27:57 +0200

    correction finale du calendrier python

--- [2018-06-20_2018-06-22]algorithmique/code/tableaufonctions/calendrier.py
+++ [2018-06-20_2018-06-22]algorithmique/code/tableaufonctions/calendrier.py
@@ -12,8 +12,26 @@ def enTete(mois, annee):
     print(" LUN MAR MER JEU VEN SAM DIM")
 
 def corps(mois, annee):
-    print(estBissextile(annee))
-    print(nbJoursMois(mois, annee))
+    #print(estBissextile(annee))
+    #print(nbJoursMois(mois, annee))
+    #print(jourDebutMois(mois, annee))
+
+    nbjours = nbJoursMois(mois, annee)
+    jourdebut = jourDebutMois(mois, annee)
+    # retrait initial
+    for retrait in range(0, jourdebut):
+        print("    ", end="")
+
+    for j in range(0, nbjours):
+        espace = "  "
+        if j < 9:
+            espace += " "
+        print(espace, j+1, sep="", end="")
+        if (j + jourdebut) % 7 == 6:
+            print("") # retour a la ligne
+
+    
+
 
 def getNomMois(mois):
     noms = ["janvier", "fevrier", "mars", "avril", "mai", "juin",
@@ -41,7 +59,23 @@ def nbJoursMois(mois, annee):
     return 0
 
 def jourDebutMois(mois, annee):
-    pass
+    # demarrage au 1er janvier 1800
+    compteur_jour = 2  # c'est un mecredi
+    #compter les jours jusqu'a la date qui nous intéresse
+    # et renvoyer une valeur entre 0 (lundi) et 6 (dimanche)
+    for a in range(1800, annee):
+        if estBissextile(a):
+            compteur_jour += 366
+        else:
+            compteur_jour += 365
+    # 1er janvier de l'annee qui nous intéresse
+    #on compte les mois restants
+    for m in range(1, mois):
+        compteur_jour += nbJoursMois(m, annee)
+    #on est sur le bon jour
+    return compteur_jour % 7
+
+
 
 
 annee = int(input("annee (apres 1800): "))
