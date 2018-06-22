15de31c62806161420024c613cd0319aec361ca2
vincent.courtalon <vincent.courtalon@gmail.com>
2018-06-22 11:38:54 +0200

    correction nbJourMois calendrier

--- [2018-06-20_2018-06-22]algorithmique/code/tableaufonctions/calendrier.py
+++ [2018-06-20_2018-06-22]algorithmique/code/tableaufonctions/calendrier.py
@@ -25,10 +25,19 @@ def getNomMois(mois):
 # True si bissextile
 # tous les 4 ans, sauf les siecles, mais quand même tous les 400 ans
 def estBissextile(annee):
-    return False
-    
+    return annee % 4 == 0 and annee % 100 != 0 or annee % 400 == 0
+
 # renvoyer le nombre de jours du mois
 def nbJoursMois(mois, annee):
+    if mois in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    if mois in [4, 6, 9, 11]:
+        return 30
+    if mois == 2:
+        if estBissextile(annee):
+            return 29
+        else:
+            return 28
     return 0
 
 def jourDebutMois(mois, annee):
