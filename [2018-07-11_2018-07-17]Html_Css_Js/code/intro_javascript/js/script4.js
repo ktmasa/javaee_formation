644118983228a218da8d01f7e93fecfe7c4abc3c
vincent.courtalon <vincent.courtalon@gmail.com>
2018-07-16 10:13:16 +0200

    introduction jquery et premier exercice

--- [2018-07-11_2018-07-17]Html_Css_Js/code/intro_javascript/js/script4.js
+++ [2018-07-11_2018-07-17]Html_Css_Js/code/intro_javascript/js/script4.js
@@ -6,4 +6,31 @@ $(document).ready(function() {
     var texte = $("#titre1").text();
     console.log("texte = " + texte);
     $("#titre1").text("bonjour depuis jquery");
+
+    //----------------------------------------
+    /*var bt1 = $("button#bt1"); // récupération bouton id bt1
+    // click (ajout de jquery) permet facilement d'ecouter un evenement html click
+    bt1.click(function() {
+        console.log("bouton 1 cliqué");
+    });*/
+
+    // appelé ajoutTexte quand bt1 est cliqué
+    $("button#bt1").click(ajoutTexte);
+    $("button#bt2").click(ajoutMessage);
+
 });
+
+function ajoutTexte() {
+    var zonetexte = $('#zonetexte');
+    zonetexte.append("<p>hello depuis ajoutexte</p>");
+
+};
+// ajoute le contenu du champ message dans la zonetexte de la page
+function ajoutMessage() {
+    var zonetexte = $('#zonetexte');
+    // récuperer le contenu du champ de saisie message
+    var message = $('input#message').val();
+    zonetexte.append("<p> vous avez dit: " + message + "</p>");
+    // ne pas soummetre le formulaire
+    return false;
+};
