09fa2d176b72a16bfe3b418764ece68abdec6234
vincent.courtalon <vincent.courtalon@gmail.com>
2018-07-19 10:29:58 +0200

    ajout edition pour produits

--- [2018-07-18_2018-07-19]Api_rest/code/exercice2_rest/js/script.js
+++ [2018-07-18_2018-07-19]Api_rest/code/exercice2_rest/js/script.js
@@ -2,7 +2,9 @@
 $(document).ready(function() {
     get_produits();
     $("#refresh").click(get_produits);
-    $("#createproduit").click(createProduit);
+    $("#createproduit").click(save_produit);
+
+    editMode = 0; // mode creation = 0, mode edition = 1
 });
 
 function deleteProduit() {
@@ -42,4 +44,58 @@ function refresh_liste(data) {
         tbody.append(produit.genererLigne());
     }
     $(".deleteproduit").click(deleteProduit);
+    $(".editproduit").click(edit_produit);
+}
+
+function edit_produit() {
+    // recupere le bouton qui a déclencher le click
+    // et dedans l'identifiant du produit stocké dans l'attribut dataid
+    var id = $(this).attr("dataid");
+    console.log("edit produit no " + id);
+    // envoie de la requette vers l'api rest
+    $.getJSON("http://localhost:3000/produits/" + id, remplir_formulaire);
+}
+
+function remplir_formulaire(data) {
+    console.log(data);
+
+    $("#id").val(data.id);
+    $("#nom").val(data.nom);
+    $("#prix").val(data.prix);
+    $("#poids").val(data.poids);
+    $("#stock").val(data.stock);
+    // passage en mode edition
+    editMode = 1;
+}
+
+function save_produit() {
+    if (editMode == 0) {
+        return createProduit();
+    }
+    else {
+        return updateProduit();
+    }
+}
+
+function updateProduit() {
+    var produit = new Produit(0);
+    if (produit.fillFromForm()) {
+        // identifiant du produit
+        var id = $("#id").val();
+        $.ajax({
+            url: "http://localhost:3000/produits/" + id,
+            type: 'PUT',
+            data: produit.toJson(),
+            success : function() {
+                editMode = 0; // on repasse en mode creation (edit fini)
+                $("#id").val(0);
+                $("#nom").val("");
+                $("#prix").val(0);
+                $("#poids").val(0);
+                $("#stock").val(0);
+                get_produits(); // rafraichir la liste
+            }
+        })
+    }
+    return false;
 }
