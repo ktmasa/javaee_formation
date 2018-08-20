3ded62f5417f3204f494d10fb43453afe055b597
vincent.courtalon <vincent.courtalon@gmail.com>
2018-08-20 09:34:56 +0200

    archetype de projet JPA

--- [2018-07-26_2018-08-08]java8/code/gestionErreur/src/gestionErreur/App.java
+++ [2018-07-26_2018-08-08]java8/code/gestionErreur/src/gestionErreur/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
 		System.out.println("quel est votre age ? ");
 	//	Scanner sc = new Scanner(System.in);
